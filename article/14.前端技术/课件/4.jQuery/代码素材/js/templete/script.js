a50be449cb88c89e04ac237c53a77b327e50bdba
zhangxue <zhangxue@bayuegua.com>
2025-11-21 10:47:52 +0800

    提交项目

--- article/14.前端技术/课件/4.jQuery/代码素材/js/templete/script.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-	alert('Welcome to StarTrackr! Now no longer under police investigation!');
-});
--- article/14.前端技术/课件/4.jQuery/代码素材/关键字查询/script.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    $("#filterName").keyup(function(){
-        $("table tbody tr").hide().filter(":contains('" + ($(this).val()) + "')").show();
-    });
-});
